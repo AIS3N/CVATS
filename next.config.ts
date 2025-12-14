6f312cdcfc66df6aa7b4bdaa46e09ebb492ccaf6
Ais3n <ais3n@protonmail.com>
2025-12-14 03:41:24 +0100

    fix deploy err

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Skip ESLint during builds (Render), prevents the requirement to have eslint installed
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
